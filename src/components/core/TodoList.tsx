c2df9b019b1ab5fb2d3dff85bbec1dac499f7a93
Kevin Aminzadeh <kevin_aminzadeh@live.com.au>
2024-04-12 18:27:44 +0930

    milestone-02 feat: improve core UI styling and layout

--- src/components/core/TodoList.tsx
+++ src/components/core/TodoList.tsx
@@ -1,16 +1,25 @@
-import { FlatList, Text } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 
 type Todo = {
 	id: string;
-	text: string;
+	title: string;
+	description?: string;
 	status: 'active' | 'completed';
 };
 
-function TodoItem({ todo }: { todo: Todo }) {
+function TodoItem({ todo, className }: { todo: Todo; className?: string }) {
 	return (
-		<Text className='text-white'>
-			{todo.text} - {todo.status}
-		</Text>
+		<View
+			className={`bg-[#2A2A2A] p-6 rounded-xl  w-full flex flex-row items-center justify-start overflow-hidden ${className}`}
+		>
+			<View className='w-6 h-6 border-slate-400 border-2 rounded-full'></View>
+
+			<View className='w-full flex flex-col pl-6'>
+				<Text className='text-white text-lg font-bold line-clamp-1'>
+					{todo.title} - {todo.status}
+				</Text>
+			</View>
+		</View>
 	);
 }
 
@@ -20,8 +29,10 @@ function TodoList({ todos }: { todos: Todo[] }) {
 			{todos?.length ? (
 				<FlatList
 					data={todos}
-					renderItem={({ item }) => <TodoItem todo={item} />}
-					className='flex flex-col'
+					renderItem={({ item, index }) => (
+						<TodoItem todo={item} className={index === 0 ? '' : 'mt-4'} />
+					)}
+					className='flex flex-col h-full w-full content-center'
 					keyExtractor={(item) => item.id}
 				/>
 			) : (
