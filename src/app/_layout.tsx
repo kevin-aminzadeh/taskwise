487582a8e6b0f155f1a8544d062474a55b24d446
Kevin Aminzadeh <kevin_aminzadeh@live.com.au>
2024-04-12 19:10:36 +0930

    final chore: implement Expo Router routing

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Slot } from 'expo-router';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+function RootLayout() {
+	return (
+		<SafeAreaProvider>
+			<Slot />
+		</SafeAreaProvider>
+	);
+}
+
+export default RootLayout;
