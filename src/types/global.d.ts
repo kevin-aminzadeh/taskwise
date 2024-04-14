4fc2de63dfe4797238a47ee9acf173dceeea899e
Kevin Aminzadeh <kevin_aminzadeh@live.com.au>
2024-04-14 23:33:04 +0930

    final refactor: update global type defs

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,6 +1,13 @@
-type Todo = {
+interface Task {
+  createdAt: number;
+  updatedAt: number;
+  completedAt?: number;
   id: string;
   title: string;
   description?: string;
   status: "active" | "completed";
-};
+}
+
+interface Tasks {
+  [key: string]: Task;
+}
