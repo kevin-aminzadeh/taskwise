8eedec0f35b6e6af008210739d984ef5886c6af5
Kevin Aminzadeh <kevin_aminzadeh@live.com.au>
2024-04-14 23:42:27 +0930

    final feat: extend TitleBar to accomodate extended use-cases

--- src/components/core/title-bar.tsx
+++ src/components/core/title-bar.tsx
@@ -1,9 +1,51 @@
-import { Text, View } from "react-native";
+import { Octicons } from "@expo/vector-icons";
+import { Link } from "expo-router";
+import { Pressable, View } from "react-native";
 
-function TitleBar({ title }: { title: string }) {
+import Heading from "./heading";
+
+type TitleBarProps = {
+  title?: string;
+  subtitle?: string;
+  secondaryNav?: React.ReactNode;
+  showBackButton?: boolean;
+};
+
+function TitleBar({
+  title,
+  subtitle,
+  secondaryNav,
+  showBackButton = false,
+}: TitleBarProps) {
   return (
-    <View className="w-full flex justify-start items-center py-12 flex-row px-8">
-      <Text className="text-white text-3xl font-extrabold">{title}</Text>
+    <View className="w-full flex flex-col justify-center items-start py-12 px-8 gap-3">
+      {subtitle && (
+        <Heading
+          text={subtitle}
+          variant="subtitle"
+          size="xl"
+          color="neutral"
+        />
+      )}
+
+      <View className="flex flex-row items-top justify-between w-full">
+        {showBackButton ? (
+          <Link href="/" asChild>
+            <Pressable className="flex flex-row items-center gap-6">
+              <Octicons
+                name="chevron-left"
+                size={24}
+                color="white"
+              />
+              <Heading text={title} size="4xl" />
+            </Pressable>
+          </Link>
+        ) : (
+          <Heading text={title} size="4xl" />
+        )}
+
+        {secondaryNav && secondaryNav}
+      </View>
     </View>
   );
 }
